d9c539295a4cbd12eb4d85e3195be2d5843383d0
Geri <luga.geri@gmail.com>
2022-03-09 18:54:29 +0100

    Created a get endpoint for all items

--- index.ts
+++ index.ts
@@ -1,6 +1,29 @@
+import express from 'express';
+import cors from 'cors';
 import { Prisma, PrismaClient } from '@prisma/client';
-const {user,item,order} = new PrismaClient({
+const { user, item, order } = new PrismaClient({
     log: ['query'],
+});
+
+const app = express();
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'PATCH', 'POST', 'DELETE', 'HEAD', 'OPTIONS', 'PUT'],
+    })
+);
+app.use(express.json());
+//items
+app.get('/items', async (req, res) => {
+    res.send(await item.findMany({include:{boughtBy:{select:{User:true,quantity:true}}}}))
+});
+
+
+
+
+
+app.listen(3009, () => {
+    console.log(`Server started on http://localhost:3009`);
 });
 
 
